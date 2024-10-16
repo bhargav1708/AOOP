84fcb430b2c1fde9ba3bffca9b8029fd1b82699d
Bhargav <bhargavnani.1708@gmail.com>
2024-10-16 21:20:48 +0530

    Add files via upload

---
+++ Smell/src/com/Smell/Coordinates.java
@@ -0,0 +1,13 @@
+package com.Smell;
+
+class Coordinates {
+    private double xCoordinate;
+    private double yCoordinate;
+    private double zCoordinate;
+
+    public Coordinates(double x, double y, double z) {
+        this.xCoordinate = x;
+        this.yCoordinate = y;
+        this.zCoordinate = z;
+    }
+}
---
+++ Smell/src/com/Smell/Main.java
@@ -0,0 +1,7 @@
+package com.Smell;
+
+public class Main {
+    public static void main(String[] args) {
+        Coordinates myCoordinates = new Coordinates(4, -7, 43);
+    }
+}
